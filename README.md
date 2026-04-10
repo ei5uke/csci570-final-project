8d3bd44a70b1aeb6a3ecdc5bd957a085e13f1d1b
eisuke <eh_352@usc.edu>
2026-04-09 17:46:30 -0700

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # csci570-final-project
 Sequence Alignment Problem: Basic DP version and the Memory-Efficient DP w/ Divide-n-conquer
+
+By: Drew Carroll and Eisuke Hirota, for CSCI570 Spring 2026
+
+## Todos
+- [ ] file completer
+    - [x] input: parse input files to get pair of sequences
+    - [ ] output: 
+- [ ] basic.py
+- [ ] efficient.py
+- [ ] summary.pdf
+- [x] 2 shell files
---
+++ efficient.py
@@ -0,0 +1,22 @@
+import sys
+from file_piping import read_input_file, write_output_file
+
+def efficient_sequence_alignment_algo(s, t):
+    """
+    Given two sequences, check for alignment
+    parameters:
+        s, t (str): a pair of sequences
+    returns:
+        ...
+    """
+
+    # TODO
+    return ""
+
+if __name__ == "__main__":
+    input_file_path = sys.argv[1]
+    output_file_path = sys.argv[2]
+
+    s, t = read_input_file(input_file_path)
+    output = efficient_sequence_alignment_algo(s, t)
+    write_output_file(output_file_path, output)
